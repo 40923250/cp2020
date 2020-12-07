40163e59c48b762885cccd1bef91dd7211e599f2
40923250 <40923250@gm.nfu.edu.tw>
2020-12-07 14:09:06 +0800

    2-1 content add

---
+++ w12.ex1.py
@@ -0,0 +1,10 @@
+import urllib.request
+url = "https://nfulist.herokuapp.com/?semester=1091&courseno=0776"
+cp1b = []
+for line in urllib.request.urlopen(url):
+    cp1b.append(int(line.decode('utf-8').rstrip()))
+print(cp1b)
+print("共" + str(len(cp1b)) + "筆")
+#print("https://github.com/" + str(cp1b) + "/cp2020" /n)
+
+    
