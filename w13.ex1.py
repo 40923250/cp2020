8feb37535819079a4d68fbca929a9191ebe52292
40923250 <40923250@gm.nfu.edu.tw>
2020-12-07 15:19:51 +0800

    2-1 modify

--- w13.ex1.py
+++ w13.ex1.py
@@ -4,7 +4,7 @@ cp1b = []
 for line in urllib.request.urlopen(url):
     cp1b.append(int(line.decode('utf-8').rstrip()))
 print(cp1b)
-#print("共" + str(len(cp1b)) + "筆")
+print("共" + str(len(cp1b)) + "筆")
 #print("https://github.com/" + str(cp1b) + "/cp2020" /n)
 def site(num):
     print("https://github.com/" + str(num) + "/cp2020")
